e3bcf878ba8d1c918a7f7af70cbec63ce6f08418
vannham92 <100520404+vannham92@users.noreply.github.com>
2026-05-01 13:56:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 
 # --- 1. CẤU HÌNH API ---
 # Thay API Key của bạn vào đây
-API_KEY = "AIzaSyAWVJemE1vdVllQdp8m4B8wlo886aMjFvI" 
+API_KEY = AIzaSyAWVJemE1vdVllQdp8m4B8wlo886aMjFvI 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
